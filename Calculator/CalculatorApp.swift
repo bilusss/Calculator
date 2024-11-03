73e3c71515b0cc6f641fd9c4dea1f4e239cf4a05
Łukasz Bilski <102826630+bilusss@users.noreply.github.com>
2024-11-03 20:22:48 +0100

    Add files via upload

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Łukasz Bilski on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
